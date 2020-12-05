5f1b8c1401122a9d9c4b0a3167b3d8a1328032eb
DYLAN HANNIGAN <dh8531@students.necsd.net>
2020-12-05 19:08:52 +0000

    i did all the lab problems

--- Prob3.java
+++ Prob3.java
@@ -2,9 +2,30 @@ import java.util.Scanner;
 
 public class Prob3  {
   public static void main(String[] args){
-    
+  Scanner scan = new Scanner(System.in);
+  System.out.println("number 0-4 pls");
 
+  int grade = scan.nextInt();
 
-    
+  if (grade == 4){
+    System.out.println("A");
+  }
+
+  else if (grade == 3){
+    System.out.println("B");
+  }
+
+  else if (grade == 2){
+    System.out.println("C");
+  }
+
+  else if (grade == 1){
+    System.out.println("D");
+  }
+
+  else {
+    System.out.println("F");
+  }
+
   }
 }
--- Prob4.java
+++ Prob4.java
@@ -3,7 +3,19 @@ import java.util.Scanner;
 public class Prob4  {
   public static void main(String[] args){
     
+    Scanner scan = new 
+Scanner(System.in);
+    System.out.println("What is the temp plz");
 
+    double temp = scan.nextDouble();
+
+    if (temp < 99 || > 102) {
+      System.out.println("WARNING");
+    }
+    
+    else {
+      System.out.println("temperature is OK");
+    }
 
     
   }
